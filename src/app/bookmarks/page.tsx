daf46c84aea5686d6c49f762200fc705a0b6674e
mwmw1023 <test5592395@naver.com>
2025-02-20 16:54:49 +0900

    page

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,6 +1,7 @@
+
 import { getAllBookmarks } from "@/lib/bookmarkApi";
-import { BookmarkType, BookmarksResponse } from "@/types";
-import Bookmarks from '@/app/components/Bookmarks';
+import { BookmarksResponse } from "@/types";
+import Bookmarks from '../components/Bookmarks';
 
 type Props = {
   searchParams: { page: string }
@@ -9,12 +10,14 @@ type Props = {
 export default async function BookmarkPage(props: Props)  {
   const { page = 1 } = props.searchParams
   const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks(parseInt(String(page)))
-  const { data } = await bookmarksData
-  const bookmarks: BookmarkType[] = data
+  const bookmarksRes = await bookmarksData
+
+  // const { data } = await bookmarksData
+  // const bookmarks: BookmarkType[] = data
 
   return (
     <div>
-        <Bookmarks bookmarks={bookmarks} />
+        <Bookmarks bookmarks={bookmarksRes} />
     </div>
   );
 }
--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,21 +1,23 @@
+import { BookmarksResponse } from '@/types';
 import React from 'react';
-import { BookmarkType } from '@/types';
 import Bookmark from './Bookmark';
+import Pagination from '@/app/components/Pagination';
 
 type Props = {
-    bookmarks: BookmarkType[]
+    bookmarks: BookmarksResponse
 }
 
 const Bookmarks: React.FC<Props> = ({bookmarks}) => {
     return (
         <div>
             <h1>Welcome to Bookmarker</h1>
+            <Pagination bookmarks={bookmarks} />
             {
-            bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
+            bookmarks.data.map(bookmark => 
+                <Bookmark key={bookmark.id} bookmark={bookmark} />)
             }            
         </div>
     );
 };
 
 export default Bookmarks;
-
